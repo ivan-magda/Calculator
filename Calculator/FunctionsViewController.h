e421aed1d8ec0f36b89ad5fb91a9a34616a93765
Ivan Magda <imagda15@gmail.com>
2014-10-03 11:00:53 +1000

    Added FunctionsViewController

---
+++ Calculator/FunctionsViewController.h
@@ -0,0 +1,15 @@
+//
+//  FunctionsViewController.h
+//  Calculator
+//
+//  Created by Ivan Magda on 03.10.14.
+//  Copyright (c) 2014 Ivan Magda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FunctionsViewController : UIViewController
+
+- (IBAction)cancelButtonPressed:(UIBarButtonItem *)sender;
+
+@end
---
+++ Calculator/FunctionsViewController.m
@@ -0,0 +1,32 @@
+//
+//  FunctionsViewController.m
+//  Calculator
+//
+//  Created by Ivan Magda on 03.10.14.
+//  Copyright (c) 2014 Ivan Magda. All rights reserved.
+//
+
+#import "FunctionsViewController.h"
+
+@interface FunctionsViewController ()
+
+@end
+
+@implementation FunctionsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)cancelButtonPressed:(UIBarButtonItem *)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+@end
